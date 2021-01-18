57099bd293466466f8016f9a829d9e2ad232f6c0
Alan Vinicius Silva <alanvinicios@gmail.com>
2021-01-18 03:09:24 -0300

    Auto git push

--- slick.js
+++ slick.js
@@ -1,34 +1,80 @@
-const $element = document.querySelector('.nabuco');
-slick($element);
+const slickState = {
+    $container: null,
+    $ul: null,
+    slidesTotal: 0,
+    slidesToShow: 3,
+    slidesToScroll: 2,
+    statesTotal: Math.ceil(12/2),
+    slideWidth: '',
+    state: 0,
+    transition: '1s',
+    infinite: false
+};
 
-function slick($element, options){
 
-    const slidesToShow = 3;
-    // const slidesToShow = options.slidesToShow;
+function slick($container, options = {}){
 
-    const itemRatio = `${(100/slidesToShow).toFixed(5)}%`;
+    slickState.$container = $container;
+    slickState.$ul = $container.querySelector("ul");
 
+    Object.assign(slickState, options);
 
-    const $ul = $element.querySelector("ul");
-    $ul.style.display = "flex";
-    $ul.style.listStyle = "none";
-    $ul.style.overflow = "hidden";
-
-    let liArray = [...$ul.querySelectorAll('li')];
-    const cloneArray = getCloneToArray(liArray);
-    $ul.append(...cloneArray);
-    liArray = $ul.querySelectorAll('li');
-
-    liArray.forEach($li => {
-        $li.style.position = "relative";
-        $li.style.flexShrink = "0";
-        $li.style.flexBasis = itemRatio;
+    // Estilização da <ul>
+    Object.assign(slickState.$ul.style, {
+        display: "flex",
+        listStyle: "none",
+        overflow: "hidden"
     });
 
+    // Largura (%) de cada <li>
+    slickState.slideWidth = 100/slickState.slidesToShow;
 
+    const liArray = [...slickState.$ul.children];
+    liArray.forEach($li => {
+        $li.style.transition = slickState.transition;
+        $li.style.flexShrink = "0";
+        $li.style.flexBasis = `${(slickState.slideWidth).toFixed(5)}%`;
+    });
+
+    insertControlButtons($container);
+}
+
+function createElement(tag, classList = [], children = []){
+    const $element = document.createElement(tag);
+    $element.classList.add(...classList);
+    $element.append(...children);
+    return $element;
+}
+
+function insertControlButtons($container){
+    const $ul = $container.firstChild;
+
+    const $leftButton = createElement("div", ["slick-button", "slick-left"], ["<"]);
+    $leftButton.addEventListener("click", slide.bind($leftButton));
+
+    const $rightButton = createElement("div", ["slick-button", "slick-right"], [">"]);
+    $rightButton.addEventListener("click", slide.bind($rightButton));
+
+    $container.insertBefore($leftButton, $ul);
+    $container.appendChild($rightButton);
+}
+
+// TODO Resolver problema de final vazio
+function slide(){
+    // Determina qual botão foi acionado
+    const moveLeft = this.classList.contains('slick-left');
+
+    const statesTotal = slickState.statesTotal;
+    slickState.state += moveLeft ? 1 : statesTotal-1;
+    slickState.state = slickState.state % statesTotal; // Ajuta o overflow
+
+    const liArray = [...slickState.$ul.children];
+    liArray.forEach($li => {
+        $li.style.transform = `translateX(-${100*slickState.slidesToScroll*slickState.state}%)`;
+    });
 }
 
 
-function getCloneToArray(elementArray){
-    return elementArray.map($element => $element.cloneNode(true));
-}
+// TODO Reomver
+const $element = document.querySelector('.nabuco');
+slick($element);
